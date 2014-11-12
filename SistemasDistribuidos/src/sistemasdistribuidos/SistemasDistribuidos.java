73945cce3d12d04824590fdd4d9a4ef6779c6987
lucashcosta <lucashcosta1@gmail.com>
2014-11-11 22:22:49 -0200

    Projeto Netbeans 8.1
    Construção da Interface

---
+++ SistemasDistribuidos/src/sistemasdistribuidos/SistemasDistribuidos.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemasdistribuidos;
+
+/**
+ *
+ * @author Lucas Hilário da Costa
+ * @author Evandro
+ * 
+ */
+public class SistemasDistribuidos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new TelaPrincipal().setVisible(true);
+    }
+    
+}
